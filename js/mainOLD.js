4f09acb16a9d2e01054d0b6c804f5ab88fcf8d10
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-07-19 02:07:27 +0400

    Added panel sliding
    Also remade some bindings.

--- js/mainOLD.js
+++ js/mainOLD.js
@@ -27,49 +27,11 @@ function showInfo() {
   }, 500);
 }
 
-function weatherOut(weather, isImperial) {
-  $('.icon').attr('class', 'icon hidden');
-  setWeatherIcon(weather.weather[0].icon);
-}
-
-
-
-// Variables, initialization and buttons events
 $(document).ready(function() {
-
   var refreshSpeed = 720000;
-  var slideSpeed = 500;
-
-  // Simple images preloading
-  var images = [];
-  var imagesLinks = [
-    'http://cs633627.vk.me/v633627450/163f/fFK--dIxZlU.jpg',
-    'http://cs633627.vk.me/v633627450/162b/691NRSEyveI.jpg',
-    'http://cs633627.vk.me/v633627450/1653/S2h58MqOwR4.jpg',
-    'http://cs628729.vk.me/v628729450/22302/qjwznie_E_g.jpg',
-    'http://cs627917.vk.me/v627917450/372f9/MQO8vGSrGVI.jpg',
-    'http://cs633627.vk.me/v633627450/1617/-MKNWboCDdM.jpg',
-    'http://cs633627.vk.me/v633627450/1667/Sv-o4MoA2zk.jpg',
-    'http://cs633627.vk.me/v633627450/160d/M557s8Wc9Rw.jpg',
-    'http://cs633627.vk.me/v633627450/165d/xQzi5fIF82U.jpg',
-  ];
-  for (var i = 0; i < imagesLinks.length; i++) {
-    images[i] = new Image();
-		images[i].src = imagesLinks[i];
-  }
 
   refreshWeather();
   var getInfo = setInterval(function() {
     refreshWeather();
   }, refreshSpeed);
-
-  $('#changeColor').on('click', function() {
-    $('.main').toggleClass('dark');
-    $('#color-btn').text($('.main').hasClass('dark') ? 'black' : 'white');
-  });
-
-  $('#back-img').on('click', function() {
-    $('.main, .back-grad').toggleClass('slideOut');
-  });
-
 });
