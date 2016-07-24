07df17ae0af55463bbd6f1afaa94af6f2e7d37b1
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-07-25 03:51:42 +0400

    Applyed normal refactoring
    Config is separated in file, view model is rewritten.

--- js/app/viewmodel.js
+++ js/app/viewmodel.js
@@ -1,41 +1,21 @@
-// TODO Test background subscribtion
 // TODO Make icon changing
-// TODO Think about exeptions
-// TODO Save weather data and hiding state in cookies
-define(['knockout', 'mapping', 'jscookie', 'app/assets', 'app/format'], (
-    knockout, mapping, Cookies, { images: loader }, format
+define(['knockout', 'mapping', 'jscookie',
+        'app/config', 'app/assets', 'app/format'], (
+        knockout, mapping, Cookies,
+        { cookie, decor, asset }, { images: loader }, format
 ) => {
   const ko = knockout;
   ko.mapping = mapping;
 
-  const resources = {
-    units: [{
-      name: 'metric',
-      temp: { convert(val) { return val - 273.15; }, symbol: 'C' },
-      speed: { convert(val) { return val; }, symbol: 'm/s' },
-    }, {
-      name: 'imperial',
-      temp: { convert(val) { return val * 9 / 5 - 459.67; }, symbol: 'F' },
-      speed: { convert(val) { return val * 2.2369363; }, symbol: 'mph' },
-    }],
-    dateFormat: ['HH:mm, DD.MM.Y', 'HH:mm, Y-MM-DD', 'h:mm P, M/D/Y'],
-  };
-
   // View model constructor
-  return function WeatherViewModel(data, assets, colors = [],
-      cfg = { tempFractionLen: 1, numFixedLen: 2 },
-      defaults = { units: 0, dateFormat: 0, colors: 0 },
-      cookie = { lifeDays: 7, dataPrefix: 'data-' }
-  ) {
-    const mapData = Object.assign({ colors }, data, resources, cfg);
+  return function WeatherViewModel(data) {
+    const mapData = Object.assign({}, data, decor);
     const mapCfg = {};
 
     // Make mapping config
     Object.keys(mapData).forEach(key => {
       mapCfg[key] = {};
-      if (Array.isArray(mapData[key])) {
-        mapCfg[key].create = elem => elem.data;
-      }
+      if (Array.isArray(mapData[key])) mapCfg[key].create = elem => elem.data;
       if (data[key] !== undefined) {
         mapCfg[key].update = elem => {
           Cookies.set(`${cookie.dataPrefix}${key}`, elem.data,
@@ -48,65 +28,54 @@ define(['knockout', 'mapping', 'jscookie', 'app/assets', 'app/format'], (
     // Map data in new view model
     const wvm = ko.mapping.fromJS(mapData, mapCfg);
 
-    // Define support functions
-    const scrollArray = name => {
-      wvm[name].push(wvm[name].shift());
-    };
-    const saveState = name => {
-      Cookies.set(name, mapData[name].findIndex(v => v === wvm[name]()[0]),
-          { expires: cookie.lifeDays });
-    };
-    const scrollAndSave = name => {
-      scrollArray(name);
-      saveState(name);
-    };
-    const setBackground = () => {
-      loader.load(assets.getUrl(wvm.icon(), wvm.temp(), wvm.speed(), wvm.dt()))
-          .then(img => { wvm.background(img.src); });
-    };
-
-    // Apply defaults
-    Object.keys(defaults).forEach(p => {
-      for (let i = Math.max(+defaults[p] || +Cookies.get(p) || 0, 0); i--;) {
-        scrollArray(p);
-      }
+    // Make computed output data formats for view model unit values
+    Object.keys(decor.units[0]).forEach(name => {
+      const compute = {
+        [`${name}Mod`]: () => wvm.units()[0][name].convert(wvm[name]()),
+        [`${name}Sign`]: () => format.numSignStr(wvm[`${name}Mod`]()),
+        [`${name}Int`]: () => `${~~wvm[`${name}Mod`]()}`,
+        [`${name}Fract`]: () => wvm[`${name}Mod`]()
+            .toFixed(wvm.fractLen()[0](wvm[`${name}Int`]().length))
+            .replace(/\d*/, ''),
+        [`${name}Val`]: () => `${wvm[`${name}Int`]()}${wvm[`${name}Fract`]()}`,
+        [`${name}Unit`]: () => wvm.units()[0][name].symbol,
+        [`${name}System`]: () => wvm.units()[0][name].sys,
+      };
+      Object.keys(compute).forEach(p => { wvm[p] = ko.computed(compute[p]); });
     });
 
-    // Compute date and time
+    // Apply defaults and define functions for decorators
+    Object.keys(decor).forEach(prop => {
+      wvm[prop].push(...wvm[prop].splice(0, +Cookies.get(prop) || 0));
+      wvm[`${prop}Next`] = function scrollAndSave() {
+        wvm[prop].push(wvm[prop].shift());
+        Cookies.set(prop, mapData[prop].findIndex(v => v === wvm[prop]()[0]),
+            { expires: cookie.lifeDays });
+      };
+    });
+
+    // Make miscellaneous computed values
+    wvm.colorScheme = ko.computed(() => wvm.colors()[0]);
+    wvm.cardinalDir = ko.computed(() => format.cardinalDir(wvm.deg()));
     wvm.date = ko.computed(() => (
         format.date(new Date(wvm.dt()), wvm.dateFormat()[0])));
 
-    // Compute temperature
-    wvm.tempMod = ko.computed(() => wvm.units()[0].temp.convert(wvm.temp()));
-    wvm.tempUnit = ko.computed(() => wvm.units()[0].temp.symbol);
-    wvm.tempInt = ko.computed(() => `${~~wvm.tempMod()}`);
-    wvm.tempFraction = ko.computed(() => (
-        wvm.tempMod().toFixed(wvm.tempFractionLen()).replace(/\d*\./, '.')));
-    wvm.tempSign = ko.computed(() => (
-        wvm.tempInt().length > 2 ? '' : format.numSignStr(wvm.tempMod())));
-
-    // Compute wind
-    wvm.speedMod = ko.computed(() => wvm.units()[0].speed.convert(wvm.speed()));
-    wvm.speedUnit = ko.computed(() => wvm.units()[0].speed.symbol);
-    wvm.speedVal = ko.computed(() => (
-        wvm.speedMod().toFixed(wvm.tempFractionLen())));
-    wvm.cardinal = ko.computed(() => format.cardinalDir(wvm.deg()));
-
-    // Define visuals
+    // Load and set image to background
     wvm.background = ko.observable('');
-    wvm.colorScheme = ko.computed(() => wvm.colors()[0]);
-    if (assets) {
-      wvm.icon.subscribe(setBackground);
-      setBackground();
-    }
+    ko.computed(() => {
+      loader.load(asset.pic.get(wvm.icon(), wvm.temp(), wvm.speed(), wvm.dt()))
+          .then(img => { wvm.background(img.src); })
+          .catch(() => { wvm.background(''); });
+    }).extend({ deferred: true });
 
-    // Define buttons
-    wvm.nextUnitSystem = () => { scrollAndSave('units'); };
-    wvm.nextDateFormat = () => { scrollAndSave('dateFormat'); };
-    wvm.nextColorScheme = () => { scrollAndSave('colors'); };
-    wvm.toggleVisibility = () => { wvm.isHidden(!wvm.isHidden()); };
+    // Define hiding functionality
+    wvm.isHidden = ko.observable(!!+Cookies.get(cookie.key.hidden) || false);
+    wvm.toggleVisibility = () => {
+      wvm.isHidden(!wvm.isHidden());
+      Cookies.set(cookie.key.hidden, +wvm.isHidden(),
+          { expires: cookie.lifeDays });
+    };
 
-    wvm.isHidden = ko.observable(false);
     return wvm;
   };
 });
--- js/main.js
+++ js/main.js
@@ -1,43 +1,22 @@
 // TODO Make exeptions on server failure
-// TODO Refactor it later, also make separate config
 // TODO Refresh timings are unstable and untested
-const apiKey = '20e6b4bb024441f12fe889046e1acbd6';
-const dataPrefix = 'data-';
-const msPerDay = 86400000;
-const refresh = { key: 'serviceRefreshed', rate: 900000 };
-
-const assets = {
-  url: [
-    [['01d'], 'http://cs633627.vk.me/v633627450/163f/fFK--dIxZlU.jpg'],
-    [['01n'], 'http://cs633627.vk.me/v633627450/162b/691NRSEyveI.jpg'],
-    [['02d'], 'http://cs633627.vk.me/v633627450/1653/S2h58MqOwR4.jpg'],
-    [['02n'], 'http://cs628729.vk.me/v628729450/22302/qjwznie_E_g.jpg'],
-    [['03d', '03n', '04d', '04n', '50d', '50n'],
-        'http://cs627917.vk.me/v627917450/372f9/MQO8vGSrGVI.jpg'],
-    [['10d', '10n'], 'http://cs633627.vk.me/v633627450/1617/-MKNWboCDdM.jpg'],
-    [['09d', '09n'], 'http://cs633627.vk.me/v633627450/1667/Sv-o4MoA2zk.jpg'],
-    [['13d', '13n'], 'http://cs633627.vk.me/v633627450/160d/M557s8Wc9Rw.jpg'],
-    [['11d', '11n'], 'http://cs633627.vk.me/v633627450/165d/xQzi5fIF82U.jpg'],
-  ],
-  getUrl(code) {
-    return this.url.find(val => val[0].includes(code))[1];
-  },
-};
-
-const amdCfg = {
+const amd = {
   baseUrl: 'js/lib',
   paths: { app: '../app' },
-  deps: ['knockout', 'mapping'],
-  callback(knockout, mapping) {
-    const ko = knockout;
-    ko.mapping = mapping;
-  },
 };
+const deps = [
+  'knockout', 'mapping', 'jscookie',
+  'app/config', 'app/weather', 'app/viewmodel',
+];
 
-require(amdCfg, ['knockout', 'jscookie', 'app/weather', 'app/viewmodel'], (
-    ko, Cookies, { OpenWeatherMap }, WeatherViewModel
+require(amd, deps, (
+    knockout, mapping, Cookies,
+    { API, cookie, MS_PER_DAY }, { OpenWeatherMap }, WeatherViewModel
 ) => {
-  const weatherService = new OpenWeatherMap(apiKey);
+  const ko = knockout;
+  ko.mapping = mapping;
+
+  const weatherService = new OpenWeatherMap(API.key);
   let weatherViewModel = {};
 
   function getWeatherData() {
@@ -48,25 +27,25 @@ require(amdCfg, ['knockout', 'jscookie', 'app/weather', 'app/viewmodel'], (
           name: city, sys: { country }, dt, main: { temp },
           weather: { 0: { icon } }, wind: { deg, speed },
         } = data;
-        Cookies.set(refresh.key, Date.now(),
-            { expires: refresh.rate / msPerDay });
+        Cookies.set(cookie.key.refresh, Date.now(),
+            { expires: API.refreshRate / MS_PER_DAY });
         return { city, country, temp, icon, speed, deg, dt: dt * 1000 };
       });
     }
 
     function getFromCookies() {
       console.log('from cookie');
-      const cookie = Cookies.get();
+      const cks = Cookies.get();
       const data = {};
-      Object.keys(cookie)
-          .filter(key => key.startsWith(dataPrefix))
+      Object.keys(cks)
+          .filter(key => key.startsWith(cookie.dataPrefix))
           .forEach(key => {
-            data[key.replace(dataPrefix, '')] = +cookie[key] || cookie[key];
+            data[key.replace(cookie.dataPrefix, '')] = +cks[key] || cks[key];
           });
       return Promise.resolve(data);
     }
 
-    return Cookies.get(refresh.key) ? getFromCookies() : getFromService();
+    return Cookies.get(cookie.key.refresh) ? getFromCookies() : getFromService();
   }
 
   function refreshData() {
@@ -74,15 +53,14 @@ require(amdCfg, ['knockout', 'jscookie', 'app/weather', 'app/viewmodel'], (
   }
 
   getWeatherData().then(data => {
-    weatherViewModel = new WeatherViewModel(data, assets,
-        ['dark', 'light', 'color']);
+    weatherViewModel = new WeatherViewModel(data);
     ko.applyBindings(weatherViewModel);
   });
 
-  console.log(refresh.rate - (Date.now() - Cookies.get(refresh.key) || 0));
+  console.log(API.refreshRate - (Date.now() - Cookies.get(cookie.key.refresh) || 0));
 
   setTimeout(() => {
     refreshData();
-    setInterval(() => { refreshData(); }, refresh.rate);
-  }, refresh.rate - (Date.now() - Cookies.get(refresh.key) || 0));
+    setInterval(() => { refreshData(); }, API.refreshRate);
+  }, API.refreshRate - (Date.now() - Cookies.get(cookie.key.refresh) || 0));
 });
