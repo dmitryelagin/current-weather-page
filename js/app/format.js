f248d68bc798ff9e9052e1347b92778e9e9b425b
Dmitry Elagin <dmitry.elagin@gmail.com>
2016-07-24 19:19:30 +0400

    Formatting functions was refactored
    Also links on page are corrected.

--- js/app/format.js
+++ js/app/format.js
@@ -1,15 +1,16 @@
 define({
+  cardinalDir(degree) {
+    const CARDINAL = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    return CARDINAL[~~((degree + 22.5) / 45)] || CARDINAL[0];
+  },
+
   numSignStr(num) {
     if (!num) return '';
     return num > 0 ? '+' : '−';
   },
 
-  numMinLen(num = 0, len = 2) {
-    return `${'0'.repeat(len)}${num}`.slice(-Math.max(len, `${num}`.length));
-  },
-
   date(dateObj = Date.now(), format = 'H:m, D.M.Y', utc = false) {
-    const DT = [
+    return [
       ['s', 'Seconds'],
       ['m', 'Minutes'],
       ['H', 'Hours'],
@@ -19,17 +20,18 @@ define({
       ['Y', 'FullYear'],
       ['P', 'Hours', date => (date >= 12 ? 'PM' : 'AM')],
       ['p', 'Hours', date => (date >= 12 ? 'p.m.' : 'a.m.')],
-    ];
-    return DT.reduce((str, args) => (
+    ].reduce((str, args) => (
         str.replace(new RegExp(`(${args[0]}+)`, 'g'), match => {
-          let value = dateObj[`get${utc ? 'UTC' : ''}${args[1]}`]();
-          if (args[2]) value = args[2](value);
-          return this.numMinLen(value, match.length);
+          let number = dateObj[`get${utc ? 'UTC' : ''}${args[1]}`]();
+          if (args[2]) number = args[2](number);
+          return `${'0'.repeat(match.length)}${number}`
+              .slice(-Math.max(match.length, `${number}`.length));
         })), format);
   },
 
-  cardinalDir(degree) {
-    const CARDINAL = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
-    return CARDINAL[~~((degree + 22.5) / 45)] || CARDINAL[0];
-  },
+  dateFormat: [
+    'HH:mm, DD.MM.Y',
+    'HH:mm, Y-MM-DD',
+    'h:mm P, M/D/Y',
+  ],
 });
